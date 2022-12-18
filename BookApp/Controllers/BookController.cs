4f1055f798e0ed2394132e495262c70d62c9210e
matheush9 <matheushenr941@gmail.com>
2022-12-17 22:59:31 -0300

    added BookController

---
+++ BookApp/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using BookApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
