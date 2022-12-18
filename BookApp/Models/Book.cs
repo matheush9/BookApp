12be463d4f7a0147efe47262da06e18be92c6edb
matheush9 <matheushenr941@gmail.com>
2022-12-17 22:59:00 -0300

    added book class model

---
+++ BookApp/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace BookApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string? Publisher { get; set; }
+        public DateOnly PublicationDate { get; set; }
+        public string Language { get; set; }
+        public int TotalPages { get; set; }
+
+    }
+
+}
