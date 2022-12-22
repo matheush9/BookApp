eadc5e1d6d86579fe79d8b3783db0b997fbf5a75
matheush9 <matheushenr941@gmail.com>
2022-12-21 23:00:57 -0300

    added app-routing

--- BookAppClient/src/app/app-routing.module.ts
+++ BookAppClient/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookComponent } from './components/book/add-book/add-book.component';
+import { BookListComponent } from './components/book/book-list/book-list.component';
+import { EditBookComponent } from './components/book/edit-book/edit-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BookListComponent
+  },
+  {
+    path: 'book',
+    component: BookListComponent
+  },
+  {
+    path: 'book/add',
+    component: AddBookComponent
+  },
+  {
+    path: 'book/edit/:id',
+    component: EditBookComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
