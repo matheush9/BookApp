91d8f076e8b2b87576e743b77869edbb958391b9
matheush9 <matheushenr941@gmail.com>
2022-12-23 22:29:01 -0300

    added app-modules

--- BookAppClient/src/app/app.module.ts
+++ BookAppClient/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from  '@angular/common/http';
+
+import {MatCardModule} from '@angular/material/card';
+
+import { BookListComponent } from './components/book/book-list/book-list.component';
+import { AddBookComponent } from './components/book/add-book/add-book.component';
+import { FormsModule } from '@angular/forms';
+import { EditBookComponent } from './components/book/edit-book/edit-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    AddBookComponent,
+    EditBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatCardModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
